ecbf265186078c8cc797cef423e1be1dd0132542
BenjiCoder02 <benkoshy02@gmail.com>
2021-07-24 21:23:57 -0400

    Added session functionality and post creation and deletion of posts

--- server.js
+++ server.js
@@ -21,25 +21,48 @@ app.use(session({
   saveUninitialized: false
 }));
 
+
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
+//Database Schemas
+
 const userSchema = new mongoose.Schema ({
+  fName: String,
+  lName: String,
   email: String,
-  password: String
+  password: String,
 });
+
+const globalSchema = new mongoose.Schema({
+  postTitle: String,
+  postContent: String,
+  postAuthor: JSON
+})
+
+
+const Global = new mongoose.model("Global", globalSchema)
 
 userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+//Passport's local session strategy
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+//Get & Post route handling
+app.get("/", function(req, res){
+  res.render("home")
+})
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -49,9 +72,15 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.get("/home", function(req, res){
+app.get("/twitterHome", function(req, res){
+  
   if (req.isAuthenticated()){
-    res.render("home");
+    Global.find(function(err, foundItems){
+      if (!err){
+        res.render("twitterHome", {posts: foundItems, name: req.user});
+      }
+    })
+    
   } else {
     res.redirect("/login");
   }
@@ -59,26 +88,31 @@ app.get("/home", function(req, res){
 
 app.get("/logout", function(req, res){
   req.logout();
-  res.redirect("/register");
+  res.redirect("/");
 });
 
 app.post("/register", function(req, res){
+  if(validateEmail(req.body.username) && req.body.password === req.body.retype_password){
 
+  
   User.register({username: req.body.username}, req.body.password, function(err, user){
     if (err) {
       console.log(err);
-      res.redirect("/register");
+      res.redirect("/login");
     } else {
       passport.authenticate("local")(req, res, function(){
-        res.redirect("/home");
+        res.redirect("/twitterHome");
       });
     }
   });
-
+  }
+  else {
+    res.render("fail")
+  }
 });
 
 app.post("/login", function(req, res){
-
+ 
   const user = new User({
     username: req.body.username,
     password: req.body.password
@@ -89,14 +123,41 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       passport.authenticate("local")(req, res, function(){
-        res.redirect("/home");
+        res.redirect("/twitterHome");
       });
     }
   });
-
+ 
 });
 
+app.get("/tweet", function(req, res){
+  res.render("tweet")
+})
 
+app.post("/tweet", function(req, res){
+  
+  Global.insertMany([{postTitle: req.body.postTitle, postContent: req.body.postContent, postAuthor: req.user}], function(err){
+    if (err){
+      console.log(err)
+    }
+    else {
+      res.redirect("twitterHome")
+    }
+  })
+})
+
+
+
+app.post("/delete", function(req, res){
+  const deleteTitle = req.body.remove
+  Global.deleteOne({ postTitle: deleteTitle }, function (err) {
+    if (!err) {
+      
+      res.redirect("/twitterHome")
+      
+    }
+  })
+})
 
 
 
@@ -105,3 +166,10 @@ app.post("/login", function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
+
+
+//Function to check email formatting
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
